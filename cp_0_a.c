4515acd7a94d7d0094c1ef85f00b209dcc623172
Jimmy Yeung <JIYE1499@cc.peralta.edu>
2019-09-05 22:55:17 +0000

    I copy the fraction code from the book, then I indent 4 times for each line of codes for a function.

--- cp_0_a.c
+++ cp_0_a.c
@@ -2,6 +2,18 @@
 
 int main(void)
 {
-  // TODO: implement addfrac.c from pg. 47
-  return 0;
+    int num1, denom1, num2, denom2, result_num, result_denom;
+  
+    printf("Enter first fraction: ");
+    scanf("%d/%d", &num1, &denom1);
+  
+    printf("Enter second fraction: ");
+    scanf("%d/%d", &num2, &denom2);
+  
+    result_num = num1 * denom2 + num2 *denom1;
+  
+    result_denom = denom1 * denom2;
+    printf("The sum is %d/%d\n", result_num, result_denom);
+  
+    return 0;
 }
